3c3b3d0dc1a734a1070e3a7c48bb8026c0091997
amfzn503 <amfzn503@msn.com>
2020-06-09 14:40:57 -0500

    assign complete working 3rd bonus

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -2,11 +2,13 @@ package org.launchcode.javawebdevtechjobsmvc.controllers;
 
 import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
+import org.launchcode.javawebdevtechjobsmvc.models.JobField;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -20,9 +22,28 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        String checkedValue = "all";
+        model.addAttribute("checkedValue", checkedValue);
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> results;
+
+        if (searchType.toLowerCase().equals("all")){
+            results = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            results = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        String checkedValue = searchType + "";
+        model.addAttribute("checkedValue", checkedValue);
+        model.addAttribute("results", results);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
 
 }
